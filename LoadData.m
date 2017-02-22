4eb385dad487041940afe8a27d1b83536daeb5c5
palanglois <thefroggy27@gmail.com>
2017-02-22 17:53:17 +0100

    Finishing SVM method implementation + beginning cross-validation

--- LoadData.m
+++ LoadData.m
@@ -1,18 +1,31 @@
 %% Load Data
 
 x_test = csvread('Xte.csv');
-x_test=x_test(:,1:end-1);
-x_train = csvread('Xtr.csv');
-x_train=x_train(:,1:end-1);
-y_train = csvread('Ytr.csv');
+x_test = x_test(:,1:end-1);
+x_train_total = csvread('Xtr.csv');
+x_train_total = x_train_total(:,1:end-1);
+y_train_total = csvread('Ytr.csv');
 
 %Moving input data to grayscale
-x_train = reshape(x_train, [5000,1024,3]);
-x_train = mean(x_train,3);
+x_train_total = reshape(x_train_total, [5000,1024,3]);
+x_train_total = mean(x_train_total,3);
 
 %Moving input data to grayscale
 x_test = reshape(x_test, [2000,1024,3]);
 x_test = mean(x_test,3);
+
+%% Cross-validation
+
+splitPart = 10;
+valPart = 1;
+
+boarderIndex = floor((splitPart-valPart)/splitPart*size(x_train_total,1));
+
+x_val = x_train_total(boarderIndex+1:end,:);
+x_train = x_train_total(1:boarderIndex,:);
+
+y_val = y_train_total(boarderIndex+1:end,:);
+y_train = y_train_total(1:boarderIndex,:);
 
 %% Train SVM
 
@@ -20,21 +33,24 @@ x_test = mean(x_test,3);
 numLabels = length(unique(y_train(:,2))); %Handle class 0
 model = cell(numLabels,1);
 for k=1:numLabels
-    model{k} = fitSVMPosterior(fitcsvm(x_train, double(y_train(:,2)==k)));
+    fprintf('Computing SVM for class %i\n',k);
+    model{k} = fitSVMPosterior(fitcsvm(x_train, double(y_train(:,2)==k-1),'KernelFonction','RBF'));
 end
 
 %% Get the posterior probability matrix for the predictions
 
 %# get probability estimates of test instances using each model
-numTest = size(x_test,1);
+numTest = size(x_val,1);
 prob = zeros(numTest,numLabels);
 for k=1:numLabels
-    [~,p] = predict(model{k}, x_test);
-    prob(:,k) = p(:,model{k}.Label==1);    %# probability of class==k
+    fprintf('Computing SVM for class %i\n',k);
+    [~,p] = predict(model{k}, x_val);
+    prob(:,k) = p(:,model{k}.ClassNames==1);    %# probability of class==k
 end
 
 %# predict the class with the highest probability
 [~,pred] = max(prob,[],2);
-acc = sum(pred == testLabel) ./ numel(testLabel)    %# accuracy
-C = confusionmat(testLabel, pred)                   %# confusion matrix
+acc = sum(pred == y_val) ./ numel(y_val);    %# accuracy
+C = confusionmat(y_val, pred);                   %# confusion matrix
+fprintf('Resulting accuracy : %f\n',acc);
 
