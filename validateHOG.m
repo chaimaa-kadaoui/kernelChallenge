f13beadd26697fa5657d1b61d104f8c7f0b447ef
Luca Grementieri <greme.luca@gmail.com>
2017-03-01 23:02:51 +0100

    Update the validation for the corrected HOG features

--- validateHOG.m
+++ validateHOG.m
@@ -2,28 +2,28 @@ function [acc, mean_acc] = validateHOG(x_train_total,y_train_total, window_size,
 
     %% Preprocess data with windowed HOG
 
-    %x_train_total_p = processHOG(x_train_total,window_size,stride, p_norm);
+    x_train_total_p = processHOG(x_train_total,window_size,stride, p_norm);
 
     %% Preprocess data with Harris corner detector based HOG
-    sigma_derivation = 1;
-    sigma_integration = 2;
-    epsilon_harris = 0.0001;
-    derivator_size = 9;
-    number_of_histo = 40;
-    c_anms = 0.99;
-    x_train_total_p = zeros(size(x_train_total,1),12*number_of_histo);
-    parfor i = 1:size(x_train_total,1)
-        % Harris corner detector
-        img_harris = harris_corner_detector(reshape(x_train_total(i,:),[32,32]),sigma_derivation,sigma_integration,epsilon_harris,derivator_size);
-        % ANMS
-        points_of_interest = anms(img_harris,number_of_histo,c_anms);
-        %HOG with centers
-        x_p = processHOGrad(x_train_total(i,:), window_size, points_of_interest, p_norm);
-        x_train_total_p(i,:) = x_p;
-        if mod(i,100) == 0
-          fprintf('Computed image : %i\n',i);
-        end
-    end
+%     sigma_derivation = 1;
+%     sigma_integration = 2;
+%     epsilon_harris = 0.0001;
+%     derivator_size = 9;
+%     number_of_histo = 40;
+%     c_anms = 0.99;
+%     x_train_total_p = zeros(size(x_train_total,1),12*number_of_histo);
+%     parfor i = 1:size(x_train_total,1)
+%         % Harris corner detector
+%         img_harris = harris_corner_detector(reshape(x_train_total(i,:),[32,32]),sigma_derivation,sigma_integration,epsilon_harris,derivator_size);
+%         % ANMS
+%         points_of_interest = anms(img_harris,number_of_histo,c_anms);
+%         %HOG with centers
+%         x_p = processHOGrad(x_train_total(i,:), window_size, points_of_interest, p_norm);
+%         x_train_total_p(i,:) = x_p;
+%         if mod(i,100) == 0
+%           fprintf('Computed image : %i\n',i);
+%         end
+%     end
     
     %% Cross-validation
 
@@ -65,8 +65,12 @@ function [acc, mean_acc] = validateHOG(x_train_total,y_train_total, window_size,
         pred = pred-1;
         acc(valPart) = sum(pred == y_val(:,2))./ numTest;         % accuracy
         fprintf('Accuracy using the validation split %i: %f\n',valPart, acc(valPart));
+        if acc<0.4
+            fprintf('Validation stopped in advance\n')
+            break
+        end
     end
 
-    mean_acc = mean(acc);
+    mean_acc = mean(acc(acc>0));
     fprintf('Average accuracy with kernel %s window_size %i and stride %i: %f\n', kernel, window_size, stride, mean_acc);
 end
--- validation.m
+++ validation.m
@@ -8,27 +8,17 @@ x_train_total = reshape(x_train_total, [size(x_train_total,1),1024,3]);
 x_train_total = mean(x_train_total,3);
 
 %% Cross validation of HOG features
-window_size = 5;
-strides = 8;
-%kernels = {'intersection','hellinger','chi2'};
-%p_norms = [1,1,0];
+params = [[6,2];[6,4];[8,2];[8,4];[8,6];[12,4];[12,5]];
 
-kernels = {'RBF'};
-p_norms = [2];
+accs = zeros(length(params),10);
+mean_accs = zeros(length(params));
 
-accs = zeros(length(strides),length(kernels),10);
-mean_accs = zeros(length(strides),length(kernels));
-
-for i=1:length(strides)
-    stride = strides(i);
-    for j=1:length(kernels)
-        kernel = char(kernels{j});
-        p_norm = p_norms(j);
-        [accs(i,j,:),mean_accs(i,j)] = validateHOG(x_train_total, y_train_total, 8, stride, kernel, p_norm);
-    end
+for l=1:length(params)
+    window_size = params(l,1);
+    stride = params(l,2);
+    [accs(l,:),mean_accs(l)] = validateHOG(x_train_total, y_train_total, window_size, stride, 'RBF', 2);
 end
 
 %% Extract best parameters
 [best_acc,p] = max(mean_accs(:));
-[best_i, best_j] = ind2sub(size(mean_accs),p);
-fprintf('Best accuracy (%f) obtained with kernel %s, window_size 8 and stride %i',best_acc, char(kernels{best_j}), strides(best_i));
+fprintf('Best accuracy (%f) obtained with window_size %i and stride %j',best_acc, params(p,1), params(p,2));
