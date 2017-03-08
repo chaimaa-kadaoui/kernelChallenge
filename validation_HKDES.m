68a96c3422bb207b0f81ea5b8a9526e7ee638186
Luca Grementieri <greme.luca@gmail.com>
2017-03-08 17:42:20 +0100

    Validation for HKDES

--- validation_HKDES.m
+++ validation_HKDES.m
@@ -1,17 +1,18 @@
 %% Load x_train_total_p from file x_HKDES.mat
-load('x_hkdes.mat')
+load('x_HKDES_realRBF.mat')
 
 %% Load data
 y_train_total = csvread('Ytr.csv');
 
 %% Cross validation of HKDES features
-params = [[800,50,800]; [800, 80, 800]; [700,30,700]; [900, 20, 900]];
+params = [[1000,200,1000]; [800,20,800]; [800, 80, 800]; [700,30,700];
+        [900, 20, 900]];
 
 accs = zeros(length(params),10);
 mean_accs = zeros(length(params),1);
 
 parfor l=1:length(params)
-	x_train_cut = x_train_total_p(:,[1:params(l,1), 2000+(1:params(l,2)), 2500+(1:params(l,3))]);
+	x_train_cut = x_train_total_p(:,[1:params(l,1), 1000+(1:params(l,2)), 1200+(1:params(l,3))]);
     [accs(l,:),mean_accs(l)] = validateHKDES(x_train_cut, y_train_total);
 end
 
